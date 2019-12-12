f1ff7eb12cf004802e31bad56a51d645a604aa94
fatimaafridi <fatimaafridi64@gmail.com>
2019-12-12 21:44:23 +0500

    retrieval of course plo map

--- src/controllers/ploCourse.js
+++ src/controllers/ploCourse.js
@@ -2,7 +2,7 @@ const log = require('../logger');
 const { Program } = require('../models').db;
 const { Plo } = require('../models').db;
 const { Course } = require('../models').db;
-
+const { CoursePlo } = require('../models').db;
 module.exports = {
   async create(req, res) {
     try {
@@ -51,6 +51,25 @@ module.exports = {
     } catch (error) {
       log.error('Create function threw an exception', error);
       return res.status(500).send({ message: 'Unexpected error occured' });
+    }
+  },
+  async retrieve(req, res) {
+    try {
+      const programId = req.params.programId;
+      let program = await Program.findByPk(programId);
+      if (!program) {
+        log.debug('Program not found');
+        return res.status(404).send({ message: 'Program not found' });
+      }
+      let list = await CoursePlo.findAll({
+        where: {
+          programId: programId
+        }
+      });
+      return res.status(200).send(list);
+    } catch (err) {
+      log.error(err);
+      return res.status(500).send({ error: err.message });
     }
   }
 };
